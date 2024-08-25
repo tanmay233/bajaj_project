60c0269f28a8db3d92f2dfe69e25696277eafedd
tanmay233 <tanmaysable.tanmay23@gmail.com>
2024-08-25 15:13:33 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+# Build the project
+echo "Building the project..."
+python3.10 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.10 manage.py collectstatic --noinput --clear
